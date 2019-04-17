21982178e8722326002c931a4a3df5b47e9ab854
Sree <mesreevidhya@gmail.com>
2019-04-17 14:16:49 +1000

    before reviews

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/restaurants", to: "restaurants#index", as: "restaurants"
+  post "/restaurants", to: "restaurants#create"
+  get "/restaurants/new", to: "restaurants#new", as: "new_restaurant"
+  get "/restaurants/:id", to: "restaurants#show", as: "restaurant"
+
+  root 'restaurants#index'
+  put "/restaurants/:id", to: "restaurants#update"
+patch "/restaurants/:id", to: "restaurants#update"
+get "/restaurants/:id/edit", to: "restaurants#edit", as: "edit_restaurant"
+end
